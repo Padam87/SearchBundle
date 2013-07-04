a0bbe5ed5300b15992f32b1bc0ae095dcb5ad850
Adam Prager <prager.adam87@gmail.com>
2013-07-04 21:31:05 +0200

    test include path

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -6,10 +6,10 @@ if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
 
 require $autoloadFile;
 
-require_once _DIR_ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Entity.php';
-require_once _DIR_ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Table.php';
-require_once _DIR_ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Id.php';
-require_once _DIR_ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Column.php';
-require_once _DIR_ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/GeneratedValue.php';
-require_once _DIR_ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/OneToMany.php';
-require_once _DIR_ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/ManyToOne.php';
+require_once __DIR__ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Entity.php';
+require_once __DIR__ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Table.php';
+require_once __DIR__ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Id.php';
+require_once __DIR__ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Column.php';
+require_once __DIR__ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/GeneratedValue.php';
+require_once __DIR__ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/OneToMany.php';
+require_once __DIR__ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/ManyToOne.php';
