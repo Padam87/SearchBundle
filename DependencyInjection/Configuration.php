5c1141d5f2a8363fbb4d2b51681de6553f9c3864
Adam Prager <prager.adam87@gmail.com>
2014-03-22 11:55:00 +0100

    Fixed SLInsight issues^2

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -16,7 +16,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        // $rootNode = $treeBuilder->root('padam87_search');
 
         return $treeBuilder;
     }
